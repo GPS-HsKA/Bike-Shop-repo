d8a38a98825151323a583c1aa005d5782943cc73
Jan Dobler <doja1011(at)hs-karlsruhe.de>
2013-10-26 13:16:41 +0200

    Aritkel geändert; Attribute erstellt

---
+++ shop/src/main/java/shop/Artikel.java
@@ -0,0 +1,6 @@
+package shop;
+
+public class Artikel {
+	private Long aId;
+
+}
--- shop/src/main/java/shop/Kunde.java
+++ shop/src/main/java/shop/Kunde.java
@@ -1,5 +1,8 @@
 package shop;
 
 public class Kunde {
+	private Long kId;
+	private String Vorname;
+	private Strin Nachname;
 
 }
