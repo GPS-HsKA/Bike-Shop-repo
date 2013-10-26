c0e4abead96806a5ea211399ca8c925c9b489362
Hans-Martin Hörtling <hoha1012 (at) hs-karlsruhe.de>
2013-10-26 13:47:23 +0200

    Mitarbeiter dem Projekt hinzugefügt

---
+++ shop/src/main/java/shop/Mittarbeiter.java
@@ -0,0 +1,22 @@
+package shop;
+
+public class Mittarbeiter {
+	
+	private String name;
+	private int personalnummer;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPersonalnummer() {
+		return personalnummer;
+	}
+	public void setPersonalnummer(int personalnummer) {
+		this.personalnummer = personalnummer;
+	}
+	
+
+}
