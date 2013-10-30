03015d0e3aa15fa7d9fe4e20c5799f2538481b41
Hans-Martin Hörtling <hoha1012 (at) hs-karlsruhe.de>
2013-10-30 20:31:50 +0100

    Artikel auflsten Rest-Rumpf
    ToDo: Methoden und Testklassen implementieren

---
+++ shop/src/main/java/de/shop/artikelverwaltung/rest/ArtikelResource.java
@@ -0,0 +1,64 @@
+package de.shop.artikelverwaltung.rest;
+
+
+//import static de.shop.util.Constants.SELF_LINK;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.MediaType.APPLICATION_XML;
+import static javax.ws.rs.core.MediaType.TEXT_XML;
+
+import java.net.URI;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Link;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import de.shop.util.rest.UriHelper;
+import de.shop.util.rest.NotFoundException;
+
+@Path("/Artikel")
+@Produces({ APPLICATION_JSON, APPLICATION_XML + ";qs=0.75", TEXT_XML + ";qs=0.5" })
+@Consumes
+public class ArtikelResource {
+	
+	@Context
+	private UriInfo uriInfo;
+	
+	@Inject
+	private UriHelper uriHelper;
+	
+	@Inject
+	private ArtikelResource ArtikelResource;
+	
+	@GET
+	@Path("{id:[1-9][0-9]*}")
+	public Response ArtikelById(@PathParam("id") Long id) {
+		
+		// TODO implementierung 
+		final Response response = Response.ok()
+                                          .build();
+		
+		return response;
+	}
+	
+	
+//	@DELETE
+//	@Path("{id:[1-9][0-9]*}/del")
+//	public Response DeleteArtikelById(@PathParam("id") Long id) {
+//	
+//		// TODO implementierung
+//		final Response responce = Response.ok().build();
+//		
+//		return responce;
+//	}
+
+	
+	
+}
