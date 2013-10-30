ab7ad8c27f16888de2288778d204523a4656e252
Alexander Schackmann <scal1029(at)hs-karlsruhe.de>
2013-10-30 17:35:25 +0100

    Bearbeitung Bestellung

---
+++ shop/src/main/java/de/shop/kundenverwaltung/rest/BestellungResource.java
@@ -0,0 +1,8 @@
+package de.shop.kundenverwaltung.rest;
+
+@Path("/bestellungen")
+@Produces({APPLICATION_JSON, APPLICATION_XML + ";qs=0.75", TEXT_XML + ";qs=0.5" })
+	
+public class BestellungResource {
+
+}
