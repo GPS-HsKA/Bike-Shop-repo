4cb3047beb793701648e49d2aa83af19e5a40ce6
Jan Dobler <doja1011(at)hs-karlsruhe.de>
2013-10-30 18:31:42 +0100

    Klasse Aritkel mit Attributen + Get/Set, Equals, HashCode, toString geschrieben

--- shop/src/main/java/de/shop/kundenverwaltung/domain/Adresse.java
+++ shop/src/main/java/de/shop/kundenverwaltung/domain/Adresse.java
@@ -1,5 +1,134 @@
 package de.shop.kundenverwaltung.domain;
 
-public class Adresse {
+import java.io.Serializable;
 
+import javax.xml.bind.annotation.XmlTransient;
+
+public class Adresse implements Serializable{	
+
+	private static final long serialVersionUID = 5993122471162770365L;
+	
+	private Long aId;
+	private String strasse;
+	private String hausNr;
+	private String plz;
+	private String ort;
+	
+	@XmlTransient
+	private AbstractKunde kunde;
+
+	public Long getaId() {
+		return aId;
+	}
+
+	public void setaId(Long aId) {
+		this.aId = aId;
+	}
+
+	public String getStrasse() {
+		return strasse;
+	}
+
+	public void setStrasse(String strasse) {
+		this.strasse = strasse;
+	}
+
+	public String getHausNr() {
+		return hausNr;
+	}
+
+	public void setHausNr(String hausNr) {
+		this.hausNr = hausNr;
+	}
+
+	public String getPlz() {
+		return plz;
+	}
+
+	public void setPlz(String plz) {
+		this.plz = plz;
+	}
+
+	public String getOrt() {
+		return ort;
+	}
+
+	public void setOrt(String ort) {
+		this.ort = ort;
+	}
+
+	public AbstractKunde getKunde() {
+		return kunde;
+	}
+
+	public void setKunde(AbstractKunde kunde) {
+		this.kunde = kunde;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((aId == null) ? 0 : aId.hashCode());
+		result = prime * result + ((hausNr == null) ? 0 : hausNr.hashCode());
+		result = prime * result + ((kunde == null) ? 0 : kunde.hashCode());
+		result = prime * result + ((ort == null) ? 0 : ort.hashCode());
+		result = prime * result + ((plz == null) ? 0 : plz.hashCode());
+		result = prime * result + ((strasse == null) ? 0 : strasse.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Adresse other = (Adresse) obj;
+		if (aId == null) {
+			if (other.aId != null)
+				return false;
+		} else if (!aId.equals(other.aId))
+			return false;
+		if (hausNr == null) {
+			if (other.hausNr != null)
+				return false;
+		} else if (!hausNr.equals(other.hausNr))
+			return false;
+		if (kunde == null) {
+			if (other.kunde != null)
+				return false;
+		} else if (!kunde.equals(other.kunde))
+			return false;
+		if (ort == null) {
+			if (other.ort != null)
+				return false;
+		} else if (!ort.equals(other.ort))
+			return false;
+		if (plz == null) {
+			if (other.plz != null)
+				return false;
+		} else if (!plz.equals(other.plz))
+			return false;
+		if (strasse == null) {
+			if (other.strasse != null)
+				return false;
+		} else if (!strasse.equals(other.strasse))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Adresse [aId=" + aId + ", strasse=" + strasse + ", hausNr="
+				+ hausNr + ", plz=" + plz + ", ort=" + ort + ", kunde=" + kunde
+				+ "]";
+	}
+	
 }
