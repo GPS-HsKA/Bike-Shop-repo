b791a0dbcc4b71c7ef633f6178c65feb76cd98b6
Jan Dobler <doja1011(at)hs-karlsruhe.de>
2013-12-19 16:06:38 +0100

    KundeService abgeändert

--- shop/src/main/java/de/shop/kundenverwaltung/service/KundeService.java
+++ shop/src/main/java/de/shop/kundenverwaltung/service/KundeService.java
@@ -1,14 +1,38 @@
 package de.shop.kundenverwaltung.service;
 
+import static de.shop.util.Constants.LOADGRAPH;
+
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+import javax.persistence.EntityGraph;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.google.common.collect.ImmutableMap;
+
+import de.shop.bestellverwaltung.domain.Bestellposition;
+import de.shop.bestellverwaltung.domain.Bestellposition_;
+import de.shop.bestellverwaltung.domain.Bestellung;
+import de.shop.bestellverwaltung.domain.Bestellung_;
 import de.shop.kundenverwaltung.domain.AbstractKunde;
-import de.shop.util.Mock;
+import de.shop.kundenverwaltung.domain.AbstractKunde_;
+import de.shop.kundenverwaltung.domain.Adresse_;
+//TODO Wartungsvertrag
+//import de.shop.kundenverwaltung.domain.Wartungsvertrag;
 import de.shop.util.interceptor.Log;
 
 @Dependent
@@ -16,59 +40,262 @@ import de.shop.util.interceptor.Log;
 public class KundeService implements Serializable {
 	private static final long serialVersionUID = 4360325837484294309L;
 	
-	@NotNull(message = "{kundenverwaltung.kunde.notFound.id}")
-	public AbstractKunde findKundeByID(Long id) {
-		if (id == null)
-			return null;
-		//TODO Datenbankzugriff muss noch gemacht werden
-		return Mock.findKundeById(id);
+	public enum FetchType {
+		NUR_KUNDE,
+		MIT_BESTELLUNGEN,
+		MIT_WARTUNGSVERTRAEGEN
+	}
 	
+	public enum OrderType {
+		KEINE,
+		ID
+	}
+	
+	@Inject
+	private transient EntityManager em;
+	
+	@NotNull(message = "{kundenverwaltung.kunde.notFound.id}")
+	public AbstractKunde findKundeById(Long id, FetchType fetch) {
+		if (id == null) {
+			return null;
+		}
+		
+		AbstractKunde kunde;
+		EntityGraph<?> entityGraph;
+		Map<String, Object> props;
+		switch (fetch) {
+			case NUR_KUNDE:
+				kunde = em.find(AbstractKunde.class, id);
+				break;
+			
+			case MIT_BESTELLUNGEN:
+				entityGraph = em.getEntityGraph(AbstractKunde.GRAPH_BESTELLUNGEN);
+				props = ImmutableMap.of(LOADGRAPH, (Object) entityGraph);
+				kunde = em.find(AbstractKunde.class, id, props);
+				break;
+//TODO Wartungsvertraege machen				
+//			case MIT_WARTUNGSVERTRAEGEN:
+//				entityGraph = em.getEntityGraph(AbstractKunde.GRAPH_WARTUNGSVERTRAEGE);
+//				props = ImmutableMap.of(LOADGRAPH, (Object) entityGraph);
+//				kunde = em.find(AbstractKunde.class, id, props);
+//				break;
+
+			default:
+				kunde = em.find(AbstractKunde.class, id);
+				break;
+		}
+		
+		return kunde;
 	}
 
-	@NotNull(message = "{kundenverwaltung.kunde.notFound.id}")
-	public AbstractKunde findKundebyEmail(String email) {
-		if (email == null)
-			return null;
-		//TODO Datenbankzugriff muss noch gemacht werden
-		return Mock.findKundeByEmail(email);
+	public List<Long> findIdsByPrefix(String idPrefix) {
+		return em.createNamedQuery(AbstractKunde.FIND_IDS_BY_PREFIX, Long.class)
+				 .setParameter(AbstractKunde.PARAM_KUNDE_ID_PREFIX, idPrefix + '%')
+				 .getResultList();
 	}
-	@Size(min = 1, message = "{kundenverwaltung.kunde.notFound.alleKunden}")
-	public List<AbstractKunde> findAllKunde() {
-		//TODO Datenbankzugriff muss noch gemacht werden
-		return Mock.findAllKunden();
+	
+	@NotNull(message = "{kundenverwaltung.kunde.notFound.id}")
+	public AbstractKunde findKundeByEmail(String email) {
+		try {
+			return em.createNamedQuery(AbstractKunde.FIND_KUNDE_BY_EMAIL, AbstractKunde.class)
+					 .setParameter(AbstractKunde.PARAM_KUNDE_EMAIL, email)
+					 .getSingleResult();
+		}
+		catch (NoResultException e) {
+			return null;
+		}
+	}
+	
+	public List<AbstractKunde> findAllKunden(FetchType fetch, OrderType order) {
+		final TypedQuery<AbstractKunde> query = OrderType.ID.equals(order)
+		                                        ? em.createNamedQuery(AbstractKunde.FIND_KUNDEN_ORDER_BY_ID,
+		                                        		              AbstractKunde.class)
+		                                        : em.createNamedQuery(AbstractKunde.FIND_KUNDEN, AbstractKunde.class);
+		
+		EntityGraph<?> entityGraph;
+		switch (fetch) {
+			case NUR_KUNDE:
+				break;
+			
+			case MIT_BESTELLUNGEN:
+				entityGraph = em.getEntityGraph(AbstractKunde.GRAPH_BESTELLUNGEN);
+				query.setHint(LOADGRAPH, entityGraph);
+				break;
+				
+//			case MIT_WARTUNGSVERTRAEGEN:
+//				entityGraph = em.getEntityGraph(AbstractKunde.GRAPH_WARTUNGSVERTRAEGE);
+//				query.setHint(LOADGRAPH, entityGraph);
+//				break;
+
+			default:
+				break;
+		}
+
+		return query.getResultList();
 	}
 	
 	@Size(min = 1, message = "{kundenverwaltung.kunde.notFound.nachname}")
-	public List<AbstractKunde> findKundeByNachname(String nachname) {
-		//TODO Datenbankzugriff muss noch gemacht werden
-		return Mock.findKundenByNachname(nachname);
+	public List<AbstractKunde> findKundenByNachname(String nachname, FetchType fetch) {
+		final TypedQuery<AbstractKunde> query = em.createNamedQuery(AbstractKunde.FIND_KUNDEN_BY_NACHNAME,
+                                                                    AbstractKunde.class)
+                                                  .setParameter(AbstractKunde.PARAM_KUNDE_NACHNAME, nachname);
+		
+		EntityGraph<?> entityGraph;
+		switch (fetch) {
+			case NUR_KUNDE:
+				break;
+				
+			case MIT_BESTELLUNGEN:
+				entityGraph = em.getEntityGraph(AbstractKunde.GRAPH_BESTELLUNGEN);
+				query.setHint(LOADGRAPH, entityGraph);
+				break;
+				
+//			case MIT_WARTUNGSVERTRAEGEN:
+//				entityGraph = em.getEntityGraph(AbstractKunde.GRAPH_WARTUNGSVERTRAEGE);
+//				query.setHint(LOADGRAPH, entityGraph);
+//				break;
+				
+			default:
+				break;
+		}
+		
+		return query.getResultList();
 	}
 	
-	public void deleteKunde(Long id) {
-		final AbstractKunde neuerKunde = findKundeByID(id);
-		if (neuerKunde == null)
-			return;
-		if (!neuerKunde.getBestellungen().isEmpty()) {
-			throw new KundeDeleteBestellungException(neuerKunde);
+	public List<String> findNachnamenByPrefix(String nachnamePrefix) {
+		return em.createNamedQuery(AbstractKunde.FIND_NACHNAMEN_BY_PREFIX, String.class)
+				 .setParameter(AbstractKunde.PARAM_KUNDE_NACHNAME_PREFIX, nachnamePrefix + '%')
+				 .getResultList();
+	}
+	
+	@Size(min = 1, message = "{kunde.notFound.plz}")
+	public List<AbstractKunde> findKundenByPLZ(String plz) {
+		return em.createNamedQuery(AbstractKunde.FIND_KUNDEN_BY_PLZ, AbstractKunde.class)
+                 .setParameter(AbstractKunde.PARAM_KUNDE_ADRESSE_PLZ, plz)
+                 .getResultList();
+	}
+	
+	@Size(min = 1, message = "{kunde.notFound.seit}")
+	public List<AbstractKunde> findKundenBySeit(Date seit) {
+		return em.createNamedQuery(AbstractKunde.FIND_KUNDEN_BY_DATE, AbstractKunde.class)
+                 .setParameter(AbstractKunde.PARAM_KUNDE_SEIT, seit)
+                 .getResultList();
+	}
+	
+	public List<AbstractKunde> findPrivatkundenFirmenkunden() {
+		return em.createNamedQuery(AbstractKunde.FIND_PRIVATKUNDEN_FIRMENKUNDEN, AbstractKunde.class)
+                 .getResultList();
+	}
+	
+	@Size(min = 1, message = "{kunde.notFound.nachname}")
+	public List<AbstractKunde> findKundenByNachnameCriteria(String nachname) {
+		final CriteriaBuilder builder = em.getCriteriaBuilder();
+		final CriteriaQuery<AbstractKunde> criteriaQuery = builder.createQuery(AbstractKunde.class);
+		final Root<AbstractKunde> k = criteriaQuery.from(AbstractKunde.class);
+
+		final Path<String> nachnamePath = k.get(AbstractKunde_.nachname);
+		//final Path<String> nachnamePath = k.get("nachname");
+		
+		final Predicate pred = builder.equal(nachnamePath, nachname);
+		criteriaQuery.where(pred);
+		
+		// Ausgabe des komponierten Query-Strings. Voraussetzung: das Modul "org.hibernate" ist aktiviert
+		//LOGGER.tracef("", em.createQuery(criteriaQuery).unwrap(org.hibernate.Query.class).getQueryString());
+		return em.createQuery(criteriaQuery).getResultList();
+	}
+	
+	@Size(min = 1, message = "{kunde.notFound.minBestMenge}")
+	public List<AbstractKunde> findKundenMitMinBestMenge(short minMenge) {
+		final CriteriaBuilder builder = em.getCriteriaBuilder();
+		final CriteriaQuery<AbstractKunde> criteriaQuery  = builder.createQuery(AbstractKunde.class);
+		final Root<AbstractKunde> k = criteriaQuery.from(AbstractKunde.class);
+
+		final Join<AbstractKunde, Bestellung> b = k.join(AbstractKunde_.bestellungen);
+		final Join<Bestellung, Bestellposition> bp = b.join(Bestellung_.bestellpositionen);
+		criteriaQuery.where(builder.gt(bp.<Short>get(Bestellposition_.anzahl), minMenge))
+		             .distinct(true);
+		
+		return em.createQuery(criteriaQuery)
+		         .getResultList();
+	}
+	
+	@NotNull(message = "{kunde.notFound.criteria}")
+	public List<AbstractKunde> findKundenByCriteria(String email, String nachname, String plz, Date seit,
+			                                        Short minBestMenge) {
+		// SELECT DISTINCT k
+		// FROM   AbstractKunde k
+		// WHERE  email = ? AND nachname = ? AND k.adresse.plz = ? and seit = ?
+		
+		final CriteriaBuilder builder = em.getCriteriaBuilder();
+		final CriteriaQuery<AbstractKunde> criteriaQuery  = builder.createQuery(AbstractKunde.class);
+		final Root<? extends AbstractKunde> k = criteriaQuery.from(AbstractKunde.class);
+		
+		Predicate pred = null;
+		if (email != null) {
+			final Path<String> emailPath = k.get(AbstractKunde_.email);
+			pred = builder.equal(emailPath, email);
 		}
-		//TODO Datenbankzugriff muss noch gemacht werden
-		Mock.deleteKunde(neuerKunde.getId());
+		if (nachname != null) {
+			final Path<String> nachnamePath = k.get(AbstractKunde_.nachname);
+			final Predicate tmpPred = builder.equal(nachnamePath, nachname);
+			pred = pred == null ? tmpPred : builder.and(pred, tmpPred);
+		}
+		if (plz != null) {
+			final Path<String> plzPath = k.get(AbstractKunde_.adresse)
+                                          .get(Adresse_.plz);
+			final Predicate tmpPred = builder.equal(plzPath, plz);
+			pred = pred == null ? tmpPred : builder.and(pred, tmpPred);
+		}
+		if (seit != null) {
+			final Path<Date> seitPath = k.get(AbstractKunde_.seit);
+			final Predicate tmpPred = builder.equal(seitPath, seit);
+			pred = pred == null ? tmpPred : builder.and(pred, tmpPred);
+		}
+		if (minBestMenge != null) {
+			final Path<Short> anzahlPath = k.join(AbstractKunde_.bestellungen)
+                                            .join(Bestellung_.bestellpositionen)
+                                            .get(Bestellposition_.anzahl);
+			final Predicate tmpPred = builder.gt(anzahlPath, minBestMenge);
+			pred = pred == null ? tmpPred : builder.and(pred, tmpPred);
+		}
+		
+		criteriaQuery.where(pred)
+		             .distinct(true);
+		return em.createQuery(criteriaQuery).getResultList();
+	}
+	
+	public void deleteKunde(AbstractKunde kunde) {
+		if (kunde == null) {
+			return;
+		}
+		
+		// Bestellungen laden, damit sie anschl. ueberprueft werden koennen
+		kunde = findKundeById(kunde.getId(), FetchType.MIT_BESTELLUNGEN);  // Kein Aufruf als Business-Methode
+		if (kunde == null) {
+			return;
+		}
+		
+		// Gibt es Bestellungen?
+		if (!kunde.getBestellungen().isEmpty()) {
+			throw new KundeDeleteBestellungException(kunde);
+		}
+
+		em.remove(kunde);
 	}
 
-	//FIXME Funktion returned null, da ein Konflikt zwischen T und AbstractKunde besteht
 	public <T extends AbstractKunde> T createKunde(T kunde) {
 		if (kunde == null) {
 			return kunde;
 		}
 
-		final AbstractKunde tmp = findKundebyEmail(kunde.getEmail());  // Kein Aufruf als Business-Methode
-		if (tmp == null) {
+		// Pruefung, ob die Email-Adresse schon existiert
+		final AbstractKunde tmp = findKundeByEmail(kunde.getEmail());   // Kein Aufruf als Business-Methode
+		if (tmp != null) {
 			throw new EmailExistsException(kunde.getEmail());
 		}
-		// TODO Datenbanzugriffsschicht statt Mock
-		kunde = Mock.createKunde(kunde);
-
-		return kunde;
+		
+		em.persist(kunde);
+		return kunde;		
 	}
 	
 	
@@ -76,19 +303,41 @@ public class KundeService implements Serializable {
 		if (kunde == null) {
 			return null;
 		}
-
-		// Pruefung, ob die Email-Adresse schon existiert
-//		final AbstractKunde vorhandenerKunde = findKundebyEmail(kunde.getEmail());  // Kein Aufruf als Business-Methode
-//		if (vorhandenerKunde != null) {
-//			 Gibt es die Email-Adresse bei einem anderen, bereits vorhandenen Kunden?
-//			if (vorhandenerKunde.getId().longValue() != kunde.getId().longValue()) {
-//				throw new EmailExistsException(kunde.getEmail());
-//			}
-//		}
-
-		// TODO Datenbanzugriffsschicht statt Mock
-		Mock.updateKunde(kunde);
 		
+		// kunde vom EntityManager trennen, weil anschliessend z.B. nach Id und Email gesucht wird
+		em.detach(kunde);
+		
+		// Gibt es ein anderes Objekt mit gleicher Email-Adresse?
+		final AbstractKunde tmp = findKundeByEmail(kunde.getEmail());  // Kein Aufruf als Business-Methode
+		if (tmp != null) {
+			em.detach(tmp);
+			if (tmp.getId().longValue() != kunde.getId().longValue()) {
+				// anderes Objekt mit gleichem Attributwert fuer email
+				throw new EmailExistsException(kunde.getEmail());
+			}
+		}
+
+		em.merge(kunde);
 		return kunde;
 	}
+//TODO Wartungsvertaege schreiben	
+//	@Size(min = 1, message = "{wartungsvertrag.notFound.kundeId}")
+//	public List<Wartungsvertrag> findWartungsvertraege(Long kundeId) {
+//		return em.createNamedQuery(Wartungsvertrag.FIND_WARTUNGSVERTRAEGE_BY_KUNDE_ID, Wartungsvertrag.class)
+//                 .setParameter(Wartungsvertrag.PARAM_KUNDE_ID, kundeId)
+//                 .getResultList();
+//	}
+//	
+//	public Wartungsvertrag createWartungsvertrag(Wartungsvertrag wartungsvertrag, AbstractKunde kunde) {
+//		if (wartungsvertrag == null || kunde == null) {
+//			return null;
+//		}
+//		
+//		kunde = findKundeById(kunde.getId(), FetchType.NUR_KUNDE);
+//		wartungsvertrag.setKunde(kunde);
+//		kunde.addWartungsvertrag(wartungsvertrag);
+//		
+//		em.persist(wartungsvertrag);
+//		return wartungsvertrag;
+//	}
 }
