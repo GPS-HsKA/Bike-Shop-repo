93a43a9d727ac015b191af6205c2b83cac4109ff
Hans-Martin Hörtling <hoha1012 (at) hs-karlsruhe.de>
2014-01-04 15:54:42 +0100

    Artikel inserts Testdaten

---
+++ shop/src/main/resources/sql/insert.sql
@@ -0,0 +1,7 @@
+-- Akrtikel Inserts
+
+INSERT INTO artikel (id, aktualisiert, erzeugt, ausgesondert, bezeichnung, preis) VALUES (301, '01-01-2014', '01-01-2014', 1, 'Testartikel' , 5.90);
+INSERT INTO artikel (id, aktualisiert, erzeugt, ausgesondert, bezeichnung, preis) VALUES (302, '01-01-2014', '01-01-2014', 0, 'Supertoller Artikel', 23.13)
+INSERT INTO artikel (id, aktualisiert, erzeugt, ausgesondert, bezeichnung, preis) VALUES (303, '01-01-2014', '01-01-2014', 0, 'Trekkingrad', 1223.13)
+INSERT INTO artikel (id, aktualisiert, erzeugt, ausgesondert, bezeichnung, preis) VALUES (304, '01-01-2014', '01-01-2014', 0, 'Rennrad', 923.13)
+INSERT INTO artikel (id, aktualisiert, erzeugt, ausgesondert, bezeichnung, preis) VALUES (305, '01-01-2014', '01-01-2014', 0, 'Mountainbike', 723.13)
