d0a354a838fce2875f5f39552d41c8069a606297
Jan Dobler <doja1011(at)hs-karlsruhe.de>
2014-01-11 13:15:04 +0100

    Unnötigen und auskommentierten Code aus Kunde gelöscht

--- shop/src/main/java/de/shop/kundenverwaltung/domain/Privatkunde.java
+++ shop/src/main/java/de/shop/kundenverwaltung/domain/Privatkunde.java
@@ -14,30 +14,4 @@ import javax.xml.bind.annotation.XmlRootElement;
 @Cacheable
 public class Privatkunde extends AbstractKunde {
 	private static final long serialVersionUID = 133152931415808605L;
-// Familienstand & Geschlecht	
-//	@Column(length = 2)
-//	//@Convert(converter = FamilienstandTypeConverter.class)
-//	private FamilienstandType familienstand = FamilienstandType.VERHEIRATET;
-//	
-//	@Column(length = 1)
-//	private GeschlechtType geschlecht = GeschlechtType.WEIBLICH;
-//	public FamilienstandType getFamilienstand() {
-//		return familienstand;
-//	}
-//	public void setFamilienstand(FamilienstandType familienstand) {
-//		this.familienstand = familienstand;
-//	}
-//
-//	public GeschlechtType getGeschlecht() {
-//		return geschlecht;
-//	}
-//	public void setGeschlecht(GeschlechtType geschlecht) {
-//		this.geschlecht = geschlecht;
-//	}
-//
-//	@Override
-//	public String toString() {
-//		return "Privatkunde [" + super.toString() + ", familienstand=" + familienstand
-//			   + ", geschlecht=" + geschlecht + ']';
-//	}
 }
